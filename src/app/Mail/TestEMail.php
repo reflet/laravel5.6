6e798fd08f840c225f9fae74708a69a77af478b6
reflet <reflet.jp@gmail.com>
2021-06-21 16:47:01 +0900

    メールのテスト送信コマンドを作ってみる。

---
+++ src/app/Mail/TestEMail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TestEMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * 注文インスタンス
+     *
+     * @var string
+     */
+    protected $body;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  string $body
+     * @return void
+     */
+    public function __construct(string $body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(): TestEMail
+    {
+        return $this
+            ->from('hoge@example.com')
+            ->view('emails.test')
+            ->with([
+                'body' => $this->body ?? ''
+            ]);
+    }
+}
